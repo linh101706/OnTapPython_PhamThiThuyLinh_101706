6e41433abe079a93237d43b5d0667a09dfb09bbc
linh101706 <linh101706@st.vimaru.edu.vn>
2024-12-10 08:55:07 +0700

    Update Linh101706.chuong2.16.py

---
+++ Linh101706.chuong2.16.py
@@ -0,0 +1,12 @@
+import math
+def isTamGiac(a, b, c):
+    if a + b > c and a + c > b and b + c > a: return True
+    else: return False
+def tinhDienTich(a, b, c):
+    p = tinhChuVi(a, b, c) / 2
+    return math.sqrt(p * (p - a) * (p - b) * (p - c))
+def tinhChuVi(a, b, c):
+    return a + b + c
+a, b, c = map(int, input().split())
+if isTamGiac(a, b, c): print("{0:.2f} {1:.2f}".format(tinhChuVi(a, b, c), tinhDienTich(a, b, c)))
+else print("Khong la tam giac")
