6011c0becf195bf56bc07cf839e8879e96aa5786
linh101706 <linh101706@st.vimaru.edu.vn>
2024-12-10 09:26:35 +0700

    Update Linh101706.chuong2.16.py

--- Linh101706.chuong2.16.py
+++ Linh101706.chuong2.16.py
@@ -9,4 +9,4 @@ def tinhChuVi(a, b, c):
     return a + b + c
 a, b, c = map(int, input().split())
 if isTamGiac(a, b, c): print("{0:.2f} {1:.2f}".format(tinhChuVi(a, b, c), tinhDienTich(a, b, c)))
-else print("Khong la tam giac")
+else: print("Khong la tam giac")
